76ccbe63a8d64bbce520a06098a787fa8be1c320
LarsWajer <116730754+LarsWajer@users.noreply.github.com>
2024-06-11 15:01:40 +0200

    updates
    kaartjes gemaakt met dummy text (homepage begin) samen met diewert gedaan

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,14 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';  // Voeg deze regel toe
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
       create: (context) => MyAppState(),
       child: MaterialApp(
         title: 'Namer App',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 83, 243, 145)),
@@ -28,24 +29,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-
-  void getNext() {
-    current = WordPair.random();
-    notifyListeners();
-  }
-
-  var favorites = <WordPair>[];
-
-  void toggleFavorite() {
-    if (favorites.contains(current)) {
-      favorites.remove(current);
-    } else {
-      favorites.add(current);
-    }
-    notifyListeners();
-  }
-
   var person;
 
   Future<void> fetchPerson() async {
@@ -105,13 +88,19 @@ class _MyHomePageState extends State<MyHomePage> {
               selectedIndex: selectedIndex,
               onDestinationSelected: (value) {
                 setState(() {
-                  selectedIndex = value;
+                  selectedIndex = (value);
                 });
               },
             ),
           ),
           Expanded(
             child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/achtergrond.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
               child: page,
             ),
           ),
@@ -124,36 +113,160 @@ class _MyHomePageState extends State<MyHomePage> {
 class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            width: 500.0,
-            height: 300.0,
-            child: Card(
-              color:  Theme.of(context).colorScheme.primaryContainer,
-            )
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            floating: false,
+            flexibleSpace: Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 10.0, 16.0, 0.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'NoBooze',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  Container(
+                    width: Theme.of(context).textTheme.headline6?.fontSize ?? 24,
+                    height: Theme.of(context).textTheme.headline6?.fontSize ?? 24,
+                    child: Image.asset('assets/images/logo.png'),
+                  ),
+                ],
+              ),
+            ),
           ),
-          Container(
-            width: 500.0,
-            height: 300.0,
-            child: Card(
-              color:  Theme.of(context).colorScheme.primaryContainer,
-            )
-          )
-          
+          SliverList(
+            delegate: SliverChildListDelegate([
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
+                child: CardWidget(
+                  title: 'Streak:',
+                  value: '5',
+                  icon: Icons.local_fire_department_sharp,
+                  iconColor: Colors.red,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
+                child: CardWidget(
+                  title: 'Dry days:',
+                  value: '2',
+                  icon: Icons.cloud,
+                  iconColor: Colors.blueGrey,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
+                child: CardWidget(
+                  title: 'Money saved:',
+                  value: '€69420,00',
+                  icon: Icons.attach_money,
+                  iconColor: Colors.black,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
+                child: ScrollableCardWidget(
+                  title: 'Motivation Quote:',
+                  value: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+                ),
+              ),
+            ]),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+
+
+class ScrollableCardWidget extends StatelessWidget {
+  final String title;
+  final String value;
+
+  const ScrollableCardWidget({
+    super.key,
+    required this.title,
+    required this.value,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 250.0,
+      height: 300.0,
+      child: Card(
+        color: Theme.of(context).colorScheme.primaryContainer,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+                SizedBox(height: 10),
+                Text(
+                  value,
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CardWidget extends StatelessWidget {
+  final String title;
+  final String value;
+  final IconData icon;
+  final Color iconColor;
+
+  const CardWidget({
+    super.key,
+    required this.title,
+    required this.value,
+    required this.icon,
+    required this.iconColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300.0,
+      height: 150.0,
+      child: Card(
+        color: Theme.of(context).colorScheme.primaryContainer,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                icon,
+                color: iconColor,
+                size: 30.0,
+              ),
+              Text(
+                title,
+                style: Theme.of(context).textTheme.headlineSmall,
+              ),
+              SizedBox(height: 10),
+              Text(
+                value,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -164,25 +277,8 @@ class FavoritesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
 
-    if (appState.favorites.isEmpty) {
-      return Center(
-        child: Text('No favorites yet.'),
-      );
-    }
-
     return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text('You have '
-              '${appState.favorites.length} favorites:'),
-        ),
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text(pair.asLowerCase),
-          ),
-      ],
+      children: [],
     );
   }
 }
