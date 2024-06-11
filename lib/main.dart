77c3b8f91f9156f7a609a9239a2cd606703b7616
diewert <s1136860@student.hsleiden.nl>
2024-06-11 12:06:38 +0200

    buttons weg gehaald. cards gemaakt
    samen met lars verder gewerkt.
    
    Co-Authored-By: LarsWajer <116730754+LarsWajer@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -138,75 +138,22 @@ class GeneratorPage extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          BigCard(pair: pair),
-          SizedBox(height: 10),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton.icon(
-                onPressed: () {
-                  appState.toggleFavorite();
-                },
-                icon: Icon(icon),
-                label: Text('Like'),
-              ),
-              SizedBox(width: 10),
-              ElevatedButton(
-                onPressed: () {
-                  appState.getNext();
-                },
-                child: Text('Next'),
-              ),
-            ],
+          Container(
+            width: 500.0,
+            height: 300.0,
+            child: Card(
+              color:  Theme.of(context).colorScheme.primaryContainer,
+            )
           ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () async {
-              await appState.fetchPerson();
-            },
-            child: Text('Fetch Person'),
-          ),
-          if (appState.person != null) ...[
-            SizedBox(height: 20),
-            Text('Name: ${appState.person['name']}'),
-            Text('Height: ${appState.person['height']}'),
-            Text('Mass: ${appState.person['mass']}'),
-            Text('Hair color: ${appState.person['hair_color']}'),
-            Text('Skin color: ${appState.person['skin_color']}'),
-            Text('Eye color: ${appState.person['eye_color']}'),
-            Text('Birth year: ${appState.person['birth_year']}'),
-            Text('Gender: ${appState.person['gender']}'),
-          ]
+          Container(
+            width: 500.0,
+            height: 300.0,
+            child: Card(
+              color:  Theme.of(context).colorScheme.primaryContainer,
+            )
+          )
+          
         ],
-      ),
-    );
-  }
-}
-
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
-
-  final WordPair pair;
-
-  @override
-  Widget build(BuildContext context) {
-    var theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
-    );
-
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Text(
-          pair.asLowerCase, 
-          style: style,
-          semanticsLabel: pair.asPascalCase,
-          ),
       ),
     );
   }
